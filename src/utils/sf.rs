23f620793663e7c14156ec55b942e154c743a570
Wesley Springer <wspringe@outlook.com>
2025-01-04 01:25:10 -0700

    figure out inner enum stuff

--- src/utils/sf.rs
+++ src/utils/sf.rs
@@ -4,6 +4,7 @@ use anyhow::anyhow;
 use anyhow::Result;
 use cli_table::format::Justify;
 use cli_table::{Cell, Style, Table, TableStruct};
+use enum_as_inner::EnumAsInner;
 use serde::Deserialize;
 use std::{
     fmt::{self, Display, Formatter},
@@ -14,7 +15,7 @@ trait SfCliResult {
     fn get_formatted_results(&self) -> TableStruct;
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TestResult {
     message: String,
@@ -23,7 +24,7 @@ struct TestResult {
     stack_trace: String,
     time: u32,
 }
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct RunTestResult {
     failures: Vec<TestResult>,
@@ -31,15 +32,15 @@ struct RunTestResult {
     num_failures: u32,
     num_tests_run: u32,
 }
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct MetadataComponent {
     component_type: String,
     full_name: String,
-    problem: String,
+    problem: Option<String>,
     success: bool,
 }
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct DeployDetails {
     component_successes: Vec<MetadataComponent>,
@@ -47,7 +48,7 @@ struct DeployDetails {
     run_test_result: RunTestResult,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct ScratchOrgInfo {
     #[serde(rename = "Id")]
     id: String,
@@ -55,7 +56,7 @@ struct ScratchOrgInfo {
     features: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, EnumAsInner, Debug)]
 #[serde(untagged)]
 enum CliResult {
     CreateScratchOrgResult {
@@ -65,12 +66,10 @@ enum CliResult {
         #[serde(rename = "orgId")]
         org_id: String,
     },
-    DeleteScratchOrgResult {
-        username: String,
-        org_id: String,
-    },
     AuthorizeResult {
         username: String,
+        #[serde(rename = "instanceUrl")]
+        instance_url: String,
     },
     ProjectDeployResult {
         details: DeployDetails,
@@ -80,6 +79,11 @@ enum CliResult {
         compile_problem: String,
         exception_message: String,
         exception_stack_trace: String,
+    },
+    DeleteScratchOrgResult {
+        username: String,
+        #[serde(rename = "orgId")]
+        org_id: String,
     },
 }
 impl SfCliResult for SfCliCommandOutput {
@@ -140,7 +144,7 @@ impl SfCliResult for SfCliCommandOutput {
             CliResult::ProjectDeployResult { details } => vec![
                 vec![
                     "Is Successful".cell(),
-                    (details.component_failures.len() != 0)
+                    (details.component_failures.len() == 0)
                         .cell()
                         .justify(Justify::Right),
                 ],
@@ -149,15 +153,11 @@ impl SfCliResult for SfCliCommandOutput {
                     details
                         .component_failures
                         .iter()
-                        .map(|x| x.problem.clone())
+                        .map(|x| x.problem.as_ref().unwrap().as_str())
                         .collect::<Vec<_>>()
                         .join("\n")
                         .cell()
                         .justify(Justify::Right),
-                ],
-                vec![
-                    "Is Deleted".cell(),
-                    (self.status == 0).cell().justify(Justify::Right),
                 ],
             ]
             .table()
@@ -458,10 +458,191 @@ mod tests {
         cli.mock_cli_output(String::from(input));
         let command_output = &cli.create_scratch_org(&"devhub".to_string(), &"alias".to_string());
         assert!(command_output.is_ok());
+
+        let result = command_output.as_ref().unwrap().result.as_ref();
+        assert!(result.is_some());
         assert!(matches!(
-            command_output.as_ref().unwrap().result.as_ref().unwrap(),
+            result.unwrap(),
             CliResult::CreateScratchOrgResult { .. }
         ));
+        assert_eq!(
+            "test@example.com",
+            result.unwrap().as_create_scratch_org_result().unwrap().0
+        );
+        assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
+    }
+
+    #[test]
+    fn it_should_delete_a_scratch_org() {
+        let input = r#"{
+          "status": 0,
+          "result": {
+            "username": "test@example.com",
+            "orgId": "001"
+          },
+          "warnings": []
+        }
+"#;
+
+        let mut cli = Cli::new();
+        cli.mock_cli_output(String::from(input));
+        let command_output = &cli.delete_old_scratch(&"test".to_string());
+        assert!(command_output.is_ok());
+
+        let result = command_output.as_ref().unwrap().result.as_ref();
+        assert!(result.is_some());
+        assert!(matches!(
+            result.unwrap(),
+            CliResult::DeleteScratchOrgResult { .. }
+        ));
+        assert_eq!(
+            "test@example.com",
+            result.unwrap().as_delete_scratch_org_result().unwrap().0
+        );
+        assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
+    }
+
+    #[test]
+    fn it_should_authorize_an_org() {
+        let input = r#"{
+  "status": 0,
+  "result": {
+    "accessToken": "token",
+    "instanceUrl": "https://test.salesforce.com",
+    "orgId": "001",
+    "username": "test.com.sandbox",
+    "loginUrl": "https://login.salesforce.com",
+    "refreshToken": "refreshToken",
+    "clientId": "PlatformCLI",
+    "isDevHub": true,
+    "instanceApiVersion": "62.0",
+    "instanceApiVersionLastRetrieved": "1/3/2025, 9:29:08 PM"
+  },
+  "warnings": []
+}
+"#;
+
+        let mut cli = Cli::new();
+        cli.mock_cli_output(String::from(input));
+        let command_output = &cli.auth_devhub(&"path".to_string());
+        assert!(command_output.is_ok());
+
+        let result = command_output.as_ref().unwrap().result.as_ref();
+        assert!(result.is_some());
+        println!("what is it, {:?}", result.unwrap());
+        assert!(matches!(result.unwrap(), CliResult::AuthorizeResult { .. }));
+        assert_eq!(
+            "test.com.sandbox",
+            result.unwrap().as_authorize_result().unwrap().0
+        );
+        assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
+    }
+
+    #[test]
+    fn it_should_deploy_metadata() {
+        let input = r#"{
+  "status": 0,
+  "result": {
+    "checkOnly": false,
+    "completedDate": "2025-01-04T07:44:01.000Z",
+    "createdBy": "005Rt00000CcLDd",
+    "createdByName": "User User",
+    "createdDate": "2025-01-04T07:44:00.000Z",
+    "details": {
+      "componentSuccesses": [
+        {
+          "changed": true,
+          "componentType": "ApexClass",
+          "created": true,
+          "createdDate": "2025-01-04T07:44:01.000Z",
+          "deleted": false,
+          "fileName": "classes/Test.cls",
+          "fullName": "Test",
+          "id": "01pRt000009wnrBIAQ",
+          "success": true
+        },
+        {
+          "changed": true,
+          "componentType": "",
+          "created": false,
+          "createdDate": "2025-01-04T07:44:01.000Z",
+          "deleted": false,
+          "fileName": "package.xml",
+          "fullName": "package.xml",
+          "success": true
+        }
+      ],
+      "runTestResult": {
+        "numFailures": 0,
+        "numTestsRun": 0,
+        "totalTime": 0,
+        "codeCoverage": [],
+        "codeCoverageWarnings": [],
+        "failures": [],
+        "flowCoverage": [],
+        "flowCoverageWarnings": [],
+        "successes": []
+      },
+      "componentFailures": []
+    },
+    "done": true,
+    "id": "0AfRt00000PqprFKAR",
+    "ignoreWarnings": false,
+    "lastModifiedDate": "2025-01-04T07:44:01.000Z",
+    "numberComponentErrors": 0,
+    "numberComponentsDeployed": 1,
+    "numberComponentsTotal": 1,
+    "numberTestErrors": 0,
+    "numberTestsCompleted": 0,
+    "numberTestsTotal": 0,
+    "rollbackOnError": true,
+    "runTestsEnabled": false,
+    "startDate": "2025-01-04T07:44:00.000Z",
+    "status": "Succeeded",
+    "success": true,
+    "files": [
+      {
+        "fullName": "Test",
+        "type": "ApexClass",
+        "state": "Created",
+        "filePath": "test/force-app/main/default/classes/Test.cls"
+      },
+      {
+        "fullName": "Test",
+        "type": "ApexClass",
+        "state": "Created",
+        "filePath": "test/force-app/main/default/classes/Test.cls-meta.xml"
+      }
+    ],
+    "zipSize": 791,
+    "zipFileCount": 3,
+    "deployUrl": "https://ability-business-62982-dev-ed.scratch.my.salesforce.com/lightning/setup/DeployStatus/page?address=%2Fchangemgmt%2FmonitorDeploymentsDetails.apexp%3FasyncId%3D0AfRt00000PqprFKAR%26retURL%3D%252Fchangemgmt%252FmonitorDeployment.apexp"
+  },
+  "warnings": []
+}
+"#;
+
+        let mut cli = Cli::new();
+        cli.mock_cli_output(String::from(input));
+        let command_output = &cli.project_deploy(&"path".to_string());
+        assert!(command_output.is_ok());
+
+        let result = command_output.as_ref().unwrap().result.as_ref();
+        assert!(result.is_some());
+        println!("what is it, {:?}", result.unwrap());
+        assert!(matches!(
+            result.unwrap(),
+            CliResult::ProjectDeployResult { .. }
+        ));
+        assert_eq!(
+            2,
+            result
+                .unwrap()
+                .as_project_deploy_result()
+                .unwrap()
+                .component_successes
+                .len()
+        );
         assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
     }
 }
