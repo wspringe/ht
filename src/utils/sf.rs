50f22d63afd3493ca68c29c3911ef0ddc9bb6dbd
Wesley Springer <wspringe@outlook.com>
2025-01-04 17:06:25 -0700

    finish positive tests

--- src/utils/sf.rs
+++ src/utils/sf.rs
@@ -16,21 +16,29 @@ trait SfCliResult {
 }
 
 #[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct TestResult {
-    message: String,
+struct RunTestResult {
+    #[serde(rename = "Outcome")]
+    outcome: String,
+    #[serde(rename = "Message")]
+    message: Option<String>,
+    #[serde(rename = "MethodName")]
     method_name: String,
-    name: String,
-    stack_trace: String,
-    time: u32,
+    #[serde(rename = "FullName")]
+    full_name: String,
+    #[serde(rename = "StackTrace")]
+    stack_trace: Option<String>,
+    #[serde(rename = "RunTime")]
+    run_time: u32,
 }
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-struct RunTestResult {
-    failures: Vec<TestResult>,
-    successes: Vec<TestResult>,
-    num_failures: u32,
-    num_tests_run: u32,
+struct RunTestSummary {
+    test_execution_time: String,
+    failing: u32,
+    fail_rate: String,
+    tests_ran: u32,
+    org_wide_coverage: String,
+    test_run_coverage: String,
 }
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -45,7 +53,6 @@ struct MetadataComponent {
 struct DeployDetails {
     component_successes: Vec<MetadataComponent>,
     component_failures: Vec<MetadataComponent>,
-    run_test_result: RunTestResult,
 }
 
 #[derive(Deserialize, Debug)]
@@ -74,6 +81,7 @@ enum CliResult {
     ProjectDeployResult {
         details: DeployDetails,
     },
+    #[serde(rename_all = "camelCase")]
     ExecuteAnonymousApexResult {
         success: bool,
         compile_problem: String,
@@ -84,6 +92,10 @@ enum CliResult {
         username: String,
         #[serde(rename = "orgId")]
         org_id: String,
+    },
+    RunApexTestsResult {
+        summary: RunTestSummary,
+        tests: Vec<RunTestResult>,
     },
 }
 impl SfCliResult for SfCliCommandOutput {
@@ -166,20 +178,62 @@ impl SfCliResult for SfCliCommandOutput {
                 "".cell(),
             ])
             .bold(true),
-            CliResult::AuthorizeResult { .. } => vec![vec![
-                "Is Authorized".cell(),
-                (self.status == 0).cell().justify(Justify::Right),
-            ]]
+            CliResult::ExecuteAnonymousApexResult {
+                compile_problem, ..
+            } => vec![
+                vec![
+                    "Compilation Successful".cell(),
+                    compile_problem.is_empty().cell().justify(Justify::Right),
+                ],
+                vec![
+                    "Problems".cell(),
+                    compile_problem.cell().justify(Justify::Right),
+                ],
+            ]
             .table()
             .title(vec![
-                "Authorize Dev Hub Results".cell().bold(true),
+                "Execute Anonymous Apex Script Results".cell().bold(true),
                 "".cell(),
             ])
             .bold(true),
-            _ => vec![vec!["Id".cell(), "id".cell().justify(Justify::Right)]]
-                .table()
-                .title(vec!["Scratch Org".cell().bold(true)])
-                .bold(true),
+            CliResult::RunApexTestsResult { summary, tests } => vec![
+                vec![
+                    "Is Successful".cell(),
+                    (summary.failing == 0).cell().justify(Justify::Right),
+                ],
+                vec![
+                    "Code Coverage".cell(),
+                    summary
+                        .org_wide_coverage
+                        .clone()
+                        .cell()
+                        .justify(Justify::Right),
+                ],
+                vec![
+                    "Failures".cell(),
+                    tests
+                        .iter()
+                        .map(|x| {
+                            if x.outcome == "Fail" {
+                                format!(
+                                    "{full_name}: {stack_trace} - {message}\n",
+                                    full_name = x.full_name,
+                                    stack_trace = x.stack_trace.clone().unwrap(),
+                                    message = x.message.clone().unwrap()
+                                )
+                            } else {
+                                String::new()
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join("\n")
+                        .cell()
+                        .justify(Justify::Right),
+                ],
+            ]
+            .table()
+            .title(vec!["Run Apex Tests Result".cell().bold(true), "".cell()])
+            .bold(true),
         }
     }
 }
@@ -189,7 +243,7 @@ pub struct SfCliCommandOutput {
     name: Option<String>,
     message: Option<String>,
     result: Option<CliResult>,
-    status: u8,
+    status: u32,
 }
 
 #[derive(Debug)]
@@ -248,7 +302,7 @@ impl Cli {
 
         let command_output: SfCliCommandOutput = serde_json::from_str(output.as_str())
             .expect("could not deserialize sf cli command output");
-        if command_output.status > 0 {
+        if command_output.status != 0 {
             return Err(anyhow!(SfCliError).context(format!(
                 "could not create scratch org: {}",
                 command_output.message.unwrap(),
@@ -262,7 +316,7 @@ impl Cli {
 
         match command_output {
             Ok(x) => Ok(String::from_utf8(x.stdout)?),
-            Err(e) => return Err(anyhow!(SfCliError).context(e.to_string())),
+            Err(e) => Err(anyhow!(SfCliError).context(e.to_string())),
         }
     }
 
@@ -282,7 +336,7 @@ impl Cli {
 
         let command_output: SfCliCommandOutput = serde_json::from_str(output.as_str())
             .expect("could not deserialize sf cli command output");
-        if command_output.status > 0 {
+        if command_output.status != 0 {
             return Err(anyhow!(SfCliError).context(format!(
                 "could not delete scratch org: {}",
                 command_output.message.unwrap(),
@@ -307,7 +361,7 @@ impl Cli {
 
         let command_output: SfCliCommandOutput = serde_json::from_str(output.as_str())
             .expect("could not deserialize sf cli command output");
-        if command_output.status > 0 {
+        if command_output.status != 0 {
             return Err(anyhow!(SfCliError).context(format!(
                 "could not authorize devhub: {}",
                 command_output.message.unwrap(),
@@ -325,9 +379,9 @@ impl Cli {
 
         let command_output: SfCliCommandOutput = serde_json::from_str(output.as_str())
             .expect("could not deserialize sf cli command output");
-        if command_output.status > 0 {
+        if command_output.status != 0 {
             return Err(anyhow!(SfCliError).context(format!(
-                "could not authorize devhub: {}",
+                "could not deploy metadata: {}",
                 command_output.message.unwrap(),
             )));
         }
@@ -343,9 +397,9 @@ impl Cli {
 
         let command_output: SfCliCommandOutput = serde_json::from_str(output.as_str())
             .expect("could not deserialize sf cli command output");
-        if command_output.status > 0 {
+        if command_output.status != 0 {
             return Err(anyhow!(SfCliError).context(format!(
-                "could not authorize devhub: {}",
+                "could not execute anonymous apex: {}",
                 command_output.message.unwrap(),
             )));
         }
@@ -361,9 +415,9 @@ impl Cli {
 
         let command_output: SfCliCommandOutput = serde_json::from_str(output.as_str())
             .expect("could not deserialize sf cli command output");
-        if command_output.status > 0 {
+        if command_output.status != 100 {
             return Err(anyhow!(SfCliError).context(format!(
-                "could not authorize devhub: {}",
+                "could not run apex tests: {}",
                 command_output.message.unwrap(),
             )));
         }
@@ -529,7 +583,6 @@ mod tests {
 
         let result = command_output.as_ref().unwrap().result.as_ref();
         assert!(result.is_some());
-        println!("what is it, {:?}", result.unwrap());
         assert!(matches!(result.unwrap(), CliResult::AuthorizeResult { .. }));
         assert_eq!(
             "test.com.sandbox",
@@ -629,7 +682,6 @@ mod tests {
 
         let result = command_output.as_ref().unwrap().result.as_ref();
         assert!(result.is_some());
-        println!("what is it, {:?}", result.unwrap());
         assert!(matches!(
             result.unwrap(),
             CliResult::ProjectDeployResult { .. }
@@ -642,6 +694,165 @@ mod tests {
                 .unwrap()
                 .component_successes
                 .len()
+        );
+        assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
+    }
+
+    #[test]
+    fn it_should_execute_anonymous_apex() {
+        let input = r#"{
+  "status": 0,
+  "result": {
+    "success": true,
+    "compiled": true,
+    "compileProblem": "",
+    "exceptionMessage": "",
+    "exceptionStackTrace": "",
+    "line": -1,
+    "column": -1,
+    "logs": "logs"
+  },
+  "warnings": []
+}
+"#;
+
+        let mut cli = Cli::new();
+        cli.mock_cli_output(String::from(input));
+        let command_output = &cli.exec_anonymous(&"path".to_string());
+        assert!(command_output.is_ok());
+
+        let result = command_output.as_ref().unwrap().result.as_ref();
+        assert!(result.is_some());
+        assert!(matches!(
+            result.unwrap(),
+            CliResult::ExecuteAnonymousApexResult { .. }
+        ));
+        assert!(
+            result
+                .unwrap()
+                .as_execute_anonymous_apex_result()
+                .unwrap()
+                .0
+        );
+        assert_eq!(
+            "",
+            result
+                .unwrap()
+                .as_execute_anonymous_apex_result()
+                .unwrap()
+                .1
+        );
+        assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
+    }
+
+    #[test]
+    fn it_should_run_apex_tests() {
+        let input = r#"{
+  "status": 100,
+  "result": {
+    "summary": {
+      "failRate": "50%",
+      "failing": 1,
+      "hostname": "https://ability-business-62982-dev-ed.scratch.my.salesforce.com",
+      "orgId": "00DRt000008pQ2HMAU",
+      "outcome": "Failed",
+      "passRate": "50%",
+      "passing": 1,
+      "skipped": 0,
+      "testRunId": "707Rt00000ZjIGa",
+      "testStartTime": "2025-01-04T22:33:54.000Z",
+      "testsRan": 2,
+      "userId": "005Rt00000CcLDdIAN",
+      "username": "test-vpfqm7c3a6cq@example.com",
+      "commandTime": "169 ms",
+      "testExecutionTime": "10 ms",
+      "testTotalTime": "10 ms",
+      "orgWideCoverage": "0%",
+      "testRunCoverage": "0%"
+    },
+    "tests": [
+      {
+        "Id": "07MRt00000AbILBMA3",
+        "QueueItemId": "709Rt00000ASqp7IAD",
+        "StackTrace": null,
+        "Message": null,
+        "AsyncApexJobId": "707Rt00000ZjIGaIAN",
+        "MethodName": "runTest",
+        "Outcome": "Pass",
+        "ApexClass": {
+          "Id": "01pRt000009xaTnIAI",
+          "Name": "TestClass",
+          "NamespacePrefix": null
+        },
+        "RunTime": 8,
+        "FullName": "TestClass.runTest"
+      },
+      {
+        "Id": "07MRt00000AbILCMA3",
+        "QueueItemId": "709Rt00000ASqp7IAD",
+        "StackTrace": "Class.TestClass.runTest2: line 10, column 1",
+        "Message": "System.AssertException: Assertion Failed: Expected: 2, Actual: 3",
+        "AsyncApexJobId": "707Rt00000ZjIGaIAN",
+        "MethodName": "runTest2",
+        "Outcome": "Fail",
+        "ApexClass": {
+          "Id": "01pRt000009xaTnIAI",
+          "Name": "TestClass",
+          "NamespacePrefix": null
+        },
+        "RunTime": 2,
+        "FullName": "TestClass.runTest2"
+      }
+    ],
+    "coverage": {
+      "coverage": [
+        {
+          "id": "01pRt000009wnrBIAQ",
+          "name": "Test",
+          "totalLines": 1,
+          "lines": {
+            "2": 0
+          },
+          "totalCovered": 0,
+          "coveredPercent": 0
+        }
+      ],
+      "records": [],
+      "summary": {
+        "totalLines": 1,
+        "coveredLines": 0,
+        "orgWideCoverage": "0%",
+        "testRunCoverage": "0%"
+      }
+    }
+  },
+  "warnings": []
+}
+"#;
+
+        let mut cli = Cli::new();
+        cli.mock_cli_output(String::from(input));
+        let command_output = &cli.run_tests();
+        assert!(command_output.is_ok());
+
+        let result = command_output.as_ref().unwrap().result.as_ref();
+        assert!(result.is_some());
+        assert!(matches!(
+            result.unwrap(),
+            CliResult::RunApexTestsResult { .. }
+        ));
+        assert_eq!(
+            1,
+            result
+                .unwrap()
+                .as_run_apex_tests_result()
+                .unwrap()
+                .0
+                .failing
+        );
+        assert_eq!(
+            2,
+            result.unwrap().as_run_apex_tests_result().unwrap().1.len()
         );
         assert!(print_stdout(command_output.as_ref().unwrap().get_formatted_results()).is_ok());
     }
